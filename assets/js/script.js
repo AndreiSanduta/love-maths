8dfd0df9bfba3360fe1af232cfa049c89df304ce
AndreiSanduta <andreisanduta@hotmail.com>
2022-06-21 20:21:55 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function rungame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementeWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function duisplaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
