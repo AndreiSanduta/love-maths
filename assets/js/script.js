b746dbfbf9a545326d68f2478f9742c9e0ea0d66
AndreiSanduta <andreisanduta@hotmail.com>
2022-06-21 20:54:19 +0000

    Add event listners for document loading adn button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function rungame() {
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame() {
 
 }
 
@@ -14,7 +32,7 @@ function incrementScore() {
 
 }
 
-function incrementeWrongAnswer() {
+function incrementWrongAnswer() {
 
 }
 
@@ -22,10 +40,10 @@ function displayAdditionQuestion() {
 
 }
 
-function duisplaySubtractQuestion() {
+function displaySubtractQuestion() {
 
 }
 
 function displayMultiplyQuestion() {
-
+    
 }
